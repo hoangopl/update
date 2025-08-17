ebd0d1a52c9f49d5d7acbc4dbbc2f9c4c2d71c36
hoangopl <gugjdidux@gmail.com>
2025-08-17 14:56:54 +0700

    overlay.cpp

--- hi.c
+++ hi.c
@@ -1,9 +1,60 @@
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
+#include <android/native_window_jni.h>
+#include <android/native_window.h>
+#include <gui/Surface.h>
+#include <gui/SurfaceComposerClient.h>
+#include <ui/DisplayInfo.h>
+#include <utils/StrongPointer.h>
+#include <unistd.h>
 
-SEC("xdp")
-int xdp_prog(struct xdp_md *ctx) {
-    return XDP_PASS;
+using namespace android;
+
+int main() {
+    // Tạo client cho SurfaceControl
+    sp<SurfaceComposerClient> client = new SurfaceComposerClient();
+
+    // Lấy thông tin màn hình
+    DisplayInfo info;
+    SurfaceComposerClient::getDisplayInfo(0, &info);
+    int width = info.w;
+    int height = info.h;
+
+    // Tạo surface (overlay)
+    sp<SurfaceControl> surfaceControl = client->createSurface(
+        String8("MyOverlay"),
+        width, height,
+        PIXEL_FORMAT_RGBA_8888,
+        ISurfaceComposerClient::eFXSurfaceBufferState |
+        ISurfaceComposerClient::eFXSurfaceDim);
+
+    // Hiển thị surface
+    SurfaceComposerClient::Transaction{}
+        .setLayer(surfaceControl, INT_MAX)   // Đặt top-most layer
+        .show(surfaceControl)
+        .apply();
+
+    // Lấy surface để vẽ
+    sp<Surface> surface = surfaceControl->getSurface();
+
+    // Vẽ màu đỏ vào toàn bộ màn hình
+    ANativeWindow_Buffer buffer;
+    surface->lock(&buffer, nullptr);
+
+    uint32_t* pixels = (uint32_t*)buffer.bits;
+    for (int y = 0; y < buffer.height; y++) {
+        for (int x = 0; x < buffer.width; x++) {
+            pixels[y * buffer.stride + x] = 0xFFFF0000; // RGBA (đỏ)
+        }
+    }
+
+    surface->unlockAndPost();
+
+    // Giữ overlay trong 5 giây
+    sleep(5);
+
+    // Ẩn surface
+    SurfaceComposerClient::Transaction{}
+        .hide(surfaceControl)
+        .apply();
+
+    return 0;
 }
-
-char _license[] SEC("license") = "GPL";
