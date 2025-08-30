9fb6b4321eda09d7708f782ed5c9e284dc926ddb
hoangopl <gugjdidux@gmail.com>
2025-08-30 10:47:04 +0700

    Update loader.c

--- loader.c
+++ loader.c
@@ -1,37 +1,41 @@
-#include <stdio.h>
-#include <bpf/libbpf.h>
+#include <linux/bpf.h>
+#include <sys/syscall.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#define BPF_OBJ_FILE "/data/local/tmp/ebpf_prog_execve.o"
 
 int main() {
-    struct bpf_object *obj;
-    int prog_fd;
-
-    // mở file prog.o (build từ prog.c)
-    obj = bpf_object__open_file("/system/etc/bpf/prog.o", NULL);
-    if (!obj) {
-        perror("bpf_object__open_file");
+    int fd = open(BPF_OBJ_FILE, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
         return 1;
     }
 
-    if (bpf_object__load(obj)) {
-        perror("bpf_object__load");
+    char buf[4096];
+    ssize_t n = read(fd, buf, sizeof(buf));
+    if (n <= 0) {
+        perror("read");
         return 1;
     }
 
-    // lấy fd của program đầu tiên
-    struct bpf_program *prog = bpf_object__next_program(obj, NULL);
-    prog_fd = bpf_program__fd(prog);
+    struct bpf_attr attr;
+    memset(&attr, 0, sizeof(attr));
+    attr.prog_type = BPF_PROG_TYPE_TRACEPOINT;
+    attr.insns = (uintptr_t)buf; // chỉ ví dụ, thực tế cần load ELF và parse section
+    attr.insn_cnt = n / sizeof(struct bpf_insn);
+    attr.license = (uintptr_t)"GPL";
 
+    int prog_fd = syscall(__NR_bpf, BPF_PROG_LOAD, &attr, sizeof(attr));
     if (prog_fd < 0) {
-        fprintf(stderr, "failed to get bpf program fd\n");
+        perror("bpf prog load");
         return 1;
     }
 
     printf("eBPF program loaded, fd=%d\n", prog_fd);
-
-    // giữ cho chương trình chạy để trace
-    while (1) {
-        sleep(5);
-    }
+    sleep(999999);
     return 0;
 }
