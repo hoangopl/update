ba1ceaf660f7fbff5e36c39669228f53c72ef329
hoangopl <gugjdidux@gmail.com>
2025-09-04 10:07:02 +0700

    Update overlay_vulkan.cpp

--- overlay_vulkan.cpp
+++ overlay_vulkan.cpp
@@ -1,5 +1,7 @@
 // overlay_vulkan.cpp
 // Android 10 - Vulkan full-screen red overlay for ~5s
+// Build inside AOSP with Android.bp / Android.mk
+// Example (outside AOSP): clang++ overlay_vulkan.cpp -o overlay_vulkan -lvulkan -llog -landroid
 
 #include <unistd.h>
 #include <stdio.h>
@@ -24,6 +26,7 @@ static void fail(const char* msg) {
 }
 
 int main(int argc, char** argv) {
+    // 1) Create a SurfaceControl
     sp<SurfaceComposerClient> client = new SurfaceComposerClient();
     if (client->initCheck() != NO_ERROR) fail("SurfaceComposerClient init failed");
 
@@ -43,25 +46,27 @@ int main(int argc, char** argv) {
     );
     if (sc == nullptr || !sc->isValid()) fail("createSurface failed");
 
+    // show and top layer
     SurfaceComposerClient::Transaction t;
     t.setLayer(sc, INT_MAX);
     t.show(sc);
     t.apply();
 
+    // 2) Obtain an ANativeWindow from the SurfaceControl (Android 10 way)
     sp<IGraphicBufferProducer> gbp = sc->getSurface();
     if (gbp == nullptr) fail("getSurface() failed");
+
     sp<Surface> surface = new Surface(gbp);
     if (surface == nullptr) fail("Surface wrapper failed");
+
     ANativeWindow* window = surface.get();
     if (window == nullptr) fail("ANativeWindow is null");
 
-    VkResult vkres;
-
+    // 3) Create Vulkan instance
     const char* instExts[] = { VK_KHR_SURFACE_EXTENSION_NAME, VK_KHR_ANDROID_SURFACE_EXTENSION_NAME };
 
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pNext = nullptr;
     appInfo.pApplicationName = "VulkanOverlay";
     appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
     appInfo.pEngineName = "None";
@@ -76,20 +81,21 @@ int main(int argc, char** argv) {
     icInfo.ppEnabledExtensionNames = instExts;
 
     VkInstance instance;
-    vkres = vkCreateInstance(&icInfo, NULL, &instance);
-    if (vkres != VK_SUCCESS) fail("vkCreateInstance failed");
+    if (vkCreateInstance(&icInfo, nullptr, &instance) != VK_SUCCESS) fail("vkCreateInstance failed");
 
+    // 4) Create VkSurfaceKHR from ANativeWindow
     VkAndroidSurfaceCreateInfoKHR surfInfo{};
     surfInfo.sType = VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR;
     surfInfo.pNext = nullptr;
     surfInfo.window = window;
 
     VkSurfaceKHR surfaceKHR;
-    vkres = vkCreateAndroidSurfaceKHR(instance, &surfInfo, NULL, &surfaceKHR);
-    if (vkres != VK_SUCCESS) fail("vkCreateAndroidSurfaceKHR failed");
+    if (vkCreateAndroidSurfaceKHR(instance, &surfInfo, nullptr, &surfaceKHR) != VK_SUCCESS)
+        fail("vkCreateAndroidSurfaceKHR failed");
 
+    // 5) Pick physical device + queue
     uint32_t gpuCount = 0;
-    vkEnumeratePhysicalDevices(instance, &gpuCount, NULL);
+    vkEnumeratePhysicalDevices(instance, &gpuCount, nullptr);
     if (gpuCount == 0) fail("No Vulkan physical devices");
 
     VkPhysicalDevice* gpus = (VkPhysicalDevice*)malloc(sizeof(VkPhysicalDevice) * gpuCount);
@@ -100,7 +106,7 @@ int main(int argc, char** argv) {
 
     for (uint32_t i = 0; i < gpuCount; ++i) {
         uint32_t qCount = 0;
-        vkGetPhysicalDeviceQueueFamilyProperties(gpus[i], &qCount, NULL);
+        vkGetPhysicalDeviceQueueFamilyProperties(gpus[i], &qCount, nullptr);
         VkQueueFamilyProperties* qProps = (VkQueueFamilyProperties*)malloc(sizeof(VkQueueFamilyProperties)*qCount);
         vkGetPhysicalDeviceQueueFamilyProperties(gpus[i], &qCount, qProps);
 
@@ -117,8 +123,9 @@ int main(int argc, char** argv) {
         if (phys != VK_NULL_HANDLE) break;
     }
     free(gpus);
-    if (phys == VK_NULL_HANDLE) fail("No suitable physical device/queue family found");
+    if (phys == VK_NULL_HANDLE) fail("No suitable GPU found");
 
+    // 6) Create logical device + queue
     float qprio = 1.0f;
     VkDeviceQueueCreateInfo dqci{};
     dqci.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
@@ -138,35 +145,38 @@ int main(int argc, char** argv) {
     devInfo.ppEnabledExtensionNames = devExts;
 
     VkDevice device;
-    vkres = vkCreateDevice(phys, &devInfo, NULL, &device);
-    if (vkres != VK_SUCCESS) fail("vkCreateDevice failed");
+    if (vkCreateDevice(phys, &devInfo, nullptr, &device) != VK_SUCCESS) fail("vkCreateDevice failed");
 
     VkQueue queue;
     vkGetDeviceQueue(device, familyIndex, 0, &queue);
 
+    // 7) Create swapchain
     VkSurfaceCapabilitiesKHR caps;
     vkGetPhysicalDeviceSurfaceCapabilitiesKHR(phys, surfaceKHR, &caps);
 
     uint32_t fmtCount = 0;
-    vkGetPhysicalDeviceSurfaceFormatsKHR(phys, surfaceKHR, &fmtCount, NULL);
+    vkGetPhysicalDeviceSurfaceFormatsKHR(phys, surfaceKHR, &fmtCount, nullptr);
     if (fmtCount == 0) fail("No surface formats");
+
     VkSurfaceFormatKHR* fmts = (VkSurfaceFormatKHR*)malloc(sizeof(VkSurfaceFormatKHR)*fmtCount);
     vkGetPhysicalDeviceSurfaceFormatsKHR(phys, surfaceKHR, &fmtCount, fmts);
     VkSurfaceFormatKHR surfFormat = fmts[0];
     free(fmts);
 
-    VkExtent2D extent = caps.currentExtent.width != UINT32_MAX ? caps.currentExtent : VkExtent2D{(uint32_t)width,(uint32_t)height};
+    VkExtent2D extent = caps.currentExtent.width != UINT32_MAX ? caps.currentExtent
+                          : VkExtent2D{(uint32_t)width,(uint32_t)height};
 
     VkSwapchainCreateInfoKHR scInfo{};
     scInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
     scInfo.pNext = nullptr;
     scInfo.surface = surfaceKHR;
-    scInfo.minImageCount = (caps.minImageCount + 1 <= caps.maxImageCount || caps.maxImageCount == 0) ? caps.minImageCount + 1 : caps.minImageCount;
+    scInfo.minImageCount = caps.minImageCount + 1 <= caps.maxImageCount || caps.maxImageCount == 0
+                           ? caps.minImageCount + 1 : caps.minImageCount;
     scInfo.imageFormat = surfFormat.format;
     scInfo.imageColorSpace = surfFormat.colorSpace;
     scInfo.imageExtent = extent;
     scInfo.imageArrayLayers = 1;
-    scInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+    scInfo.imageUsage = VK_IMAGE_USAGE_TRANSFER_DST_BIT;
     scInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
     scInfo.preTransform = caps.currentTransform;
     scInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
@@ -174,23 +184,23 @@ int main(int argc, char** argv) {
     scInfo.clipped = VK_TRUE;
 
     VkSwapchainKHR swapchain;
-    vkres = vkCreateSwapchainKHR(device, &scInfo, NULL, &swapchain);
-    if (vkres != VK_SUCCESS) fail("vkCreateSwapchainKHR failed");
+    if (vkCreateSwapchainKHR(device, &scInfo, nullptr, &swapchain) != VK_SUCCESS)
+        fail("vkCreateSwapchainKHR failed");
 
+    // 8) Acquire images
     uint32_t imgCount = 0;
-    vkGetSwapchainImagesKHR(device, swapchain, &imgCount, NULL);
+    vkGetSwapchainImagesKHR(device, swapchain, &imgCount, nullptr);
     VkImage* images = (VkImage*)malloc(sizeof(VkImage)*imgCount);
     vkGetSwapchainImagesKHR(device, swapchain, &imgCount, images);
 
+    // Command pool + buffer
     VkCommandPoolCreateInfo cpci{};
     cpci.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
     cpci.pNext = nullptr;
     cpci.queueFamilyIndex = familyIndex;
-    cpci.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
 
     VkCommandPool cmdPool;
-    vkres = vkCreateCommandPool(device, &cpci, NULL, &cmdPool);
-    if (vkres != VK_SUCCESS) fail("vkCreateCommandPool failed");
+    vkCreateCommandPool(device, &cpci, nullptr, &cmdPool);
 
     VkCommandBufferAllocateInfo cbai{};
     cbai.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
@@ -200,27 +210,29 @@ int main(int argc, char** argv) {
     cbai.commandBufferCount = 1;
 
     VkCommandBuffer cmdBuf;
-    vkres = vkAllocateCommandBuffers(device, &cbai, &cmdBuf);
-    if (vkres != VK_SUCCESS) fail("vkAllocateCommandBuffers failed");
+    vkAllocateCommandBuffers(device, &cbai, &cmdBuf);
 
+    // Fence
     VkFenceCreateInfo fci{};
     fci.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
     fci.pNext = nullptr;
 
     VkFence fence;
-    vkCreateFence(device, &fci, NULL, &fence);
+    vkCreateFence(device, &fci, nullptr, &fence);
 
+    // Semaphores
     VkSemaphoreCreateInfo sci{};
     sci.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
     sci.pNext = nullptr;
 
     VkSemaphore imageAvail, renderDone;
-    vkCreateSemaphore(device, &sci, NULL, &imageAvail);
-    vkCreateSemaphore(device, &sci, NULL, &renderDone);
+    vkCreateSemaphore(device, &sci, nullptr, &imageAvail);
+    vkCreateSemaphore(device, &sci, nullptr, &renderDone);
 
     uint32_t imgIndex = 0;
     vkAcquireNextImageKHR(device, swapchain, UINT64_MAX, imageAvail, VK_NULL_HANDLE, &imgIndex);
 
+    // Record command buffer: clear to red
     VkCommandBufferBeginInfo cbbi{};
     cbbi.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
     cbbi.pNext = nullptr;
@@ -236,33 +248,32 @@ int main(int argc, char** argv) {
     barrier.dstQueueFamilyIndex = VK_QUEUE_FAMILY_IGNORED;
     barrier.image = images[imgIndex];
     barrier.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-    barrier.subresourceRange.baseMipLevel = 0;
     barrier.subresourceRange.levelCount = 1;
-    barrier.subresourceRange.baseArrayLayer = 0;
     barrier.subresourceRange.layerCount = 1;
-    barrier.srcAccessMask = 0;
     barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
 
     vkCmdPipelineBarrier(cmdBuf,
                          VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT,
-                         0, 0, NULL, 0, NULL, 1, &barrier);
+                         0, 0, nullptr, 0, nullptr, 1, &barrier);
 
-    VkClearColorValue clearColor = {{1.0f, 0.0f, 0.0f, 1.0f}};
-    VkImageSubresourceRange range = barrier.subresourceRange;
-    vkCmdClearColorImage(cmdBuf, images[imgIndex], VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &clearColor, 1, &range);
+    VkClearColorValue clearColor = {{1.f, 0.f, 0.f, 1.f}};
+    vkCmdClearColorImage(cmdBuf, images[imgIndex],
+                         VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                         &clearColor, 1, &barrier.subresourceRange);
 
     barrier.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
     barrier.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
     barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
-    barrier.dstAccessMask = 0;
 
     vkCmdPipelineBarrier(cmdBuf,
                          VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-                         0, 0, NULL, 0, NULL, 1, &barrier);
+                         0, 0, nullptr, 0, nullptr, 1, &barrier);
 
     vkEndCommandBuffer(cmdBuf);
 
+    // Submit + present
     VkPipelineStageFlags waitStages[] = { VK_PIPELINE_STAGE_TRANSFER_BIT };
+
     VkSubmitInfo submit{};
     submit.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
     submit.pNext = nullptr;
@@ -276,7 +287,6 @@ int main(int argc, char** argv) {
 
     vkQueueSubmit(queue, 1, &submit, fence);
     vkWaitForFences(device, 1, &fence, VK_TRUE, UINT64_MAX);
-    vkResetFences(device, 1, &fence);
 
     VkPresentInfoKHR pres{};
     pres.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
@@ -289,19 +299,21 @@ int main(int argc, char** argv) {
 
     vkQueuePresentKHR(queue, &pres);
 
+    // Keep overlay for 5 seconds
     sleep(5);
 
+    // Cleanup
     vkDeviceWaitIdle(device);
-    vkDestroySemaphore(device, renderDone, NULL);
-    vkDestroySemaphore(device, imageAvail, NULL);
-    vkDestroyFence(device, fence, NULL);
+    vkDestroySemaphore(device, renderDone, nullptr);
+    vkDestroySemaphore(device, imageAvail, nullptr);
+    vkDestroyFence(device, fence, nullptr);
     vkFreeCommandBuffers(device, cmdPool, 1, &cmdBuf);
-    vkDestroyCommandPool(device, cmdPool, NULL);
+    vkDestroyCommandPool(device, cmdPool, nullptr);
     free(images);
-    vkDestroySwapchainKHR(device, swapchain, NULL);
-    vkDestroyDevice(device, NULL);
-    vkDestroySurfaceKHR(instance, surfaceKHR, NULL);
-    vkDestroyInstance(instance, NULL);
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
+    vkDestroyDevice(device, nullptr);
+    vkDestroySurfaceKHR(instance, surfaceKHR, nullptr);
+    vkDestroyInstance(instance, nullptr);
 
     SurfaceComposerClient::Transaction cleanup;
     cleanup.hide(sc);
